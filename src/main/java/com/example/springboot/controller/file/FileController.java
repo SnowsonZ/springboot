25b48a3b8c565373a9067c904a42783809d4262d
snowson <snowsonz@163.com>
2018-06-24 23:54:31 +0800

    添加多文件上传接口

--- src/main/java/com/example/springboot/controller/file/FileController.java
+++ src/main/java/com/example/springboot/controller/file/FileController.java
@@ -5,10 +5,13 @@ import com.example.springboot.utils.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * @Author: Snowson
@@ -45,10 +48,31 @@ public class FileController {
         String filePath = FileUtils.PATH_FILE_UPLOAD + fileKeyPath;
         try {
             FileUtils.uploadFile(file.getBytes(), filePath, photoName);
+            String result = "{\"status\":\"upload success!!\"}";
+//            String result = "success...";
+            return result;
         } catch (Exception e) {
             e.printStackTrace();
             logger.error("获取文件字节码出错...");
+            String result = "{\"status\":\"upload failed...\"}";
+//            String result = "failed...";
+            return result;
         }
-        return "upload success!!!";
+    }
+
+    @PostMapping("list")
+    public String uploadFileList(@RequestParam("desc") String desc, @RequestBody List<MultipartFile> files) {
+        String fileKeyPath = System.currentTimeMillis() + "/";
+        for (MultipartFile file : files) {
+            String fileName = System.currentTimeMillis() + "." + file.getOriginalFilename().split("\\.")[1];
+            String filePath = FileUtils.PATH_FILE_UPLOAD + fileKeyPath;
+            try {
+                FileUtils.uploadFile(file.getBytes(), filePath, fileName);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "{\"status\":\"upload failed...\"}";
+            }
+        }
+        return "{\"status\":\"upload success...\"}";
     }
 }
